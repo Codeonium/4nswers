a82982c6c5139d35906dc6fb69376ec5cee69554
StuMcA <stuart.mcara@outlook.com>
2021-05-28 21:26:57 +0100

    input refactor

--- src/components/PlayerInput.js
+++ src/components/PlayerInput.js
@@ -1,11 +1,30 @@
-const PlayerInput = ({playerInput, placeholder, handleInputChange}) => {
+import {useState, useEffect} from 'react';
+
+const PlayerInput = ({gameRound, playerInput, placeholder, handleInputChange}) => {
+
+    const [inputArray, setInputArray] = useState([]);
 
     const handleKeyPress = (event) => {
         handleInputChange(event);
     }
+    
+    const updateInputArray = () => {
+        for (let i = 1; 1 <= gameRound; i++) {
+            setInputArray([...inputArray, 
+                <input type="text" placeholder="*" onKeyUp={handleKeyPress} value={playerInput} onChange={function() {}} />
+            ])
+        }
+    }
+
+    useEffect(() => {
+        updateInputArray();
+    }, [gameRound])
+
 
     return (
-        <input type="text" placeholder={placeholder} onKeyUp={handleKeyPress} value={playerInput} onChange={function() {}} autoFocus/>
+        <>
+            {inputArray}
+        </>
     );
 }
 
